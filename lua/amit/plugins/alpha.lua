c15551a628628eabb7c8bd01ccad3d5454b8a2c6
Amit-Weis <amitweisgor@gmail.com>
2024-08-12 18:21:58 -0400

    more bonsai work

--- lua/amit/plugins/alpha.lua
+++ lua/amit/plugins/alpha.lua
@@ -1,108 +1,184 @@
-local count = 0
-local function ansi_truecolor_to_hex(ansi_code)
-	-- Split the ANSI code by ";"
-	local parts = {}
-	for part in string.gmatch(ansi_code, "([^;]+)") do
-		table.insert(parts, part)
-	end
+-- Open the file and generate the tree
+local file = io.open("tree.txt", "w+")
+os.execute("pybonsai > tree.txt")
+file:close() -- Close the file after writing
 
-	-- Convert parts to integers and then to hex
-	local r = string.format("%02X", tonumber(parts[#parts - 2]))
-	local g = string.format("%02X", tonumber(parts[#parts - 1]))
-	local b = string.format("%02X", tonumber(parts[#parts]))
-
-	-- Return hex color code
-	return string.format("#%s%s%s", r, g, b)
+-- Reopen the file to read its content
+file = io.open("tree.txt", "r")
+local lines = {}
+for line in file:lines() do
+	table.insert(lines, line)
 end
+file:close() -- Close the file after reading
 
-local function hex_to_vim_highlight(color)
-	-- Create Vim highlight command
+-- Define the function to strip the tree boundaries and write to another file
+local function strip_tree_boundaries_to_file(input_lines, output_file)
+	local first_escape_line = -1
+	local last_escape_line = -1
+	local escape_char = "\27" -- This is the escape character (ASCII 27)
 
-	local vim_command = string.format(":highlight %s guifg=%s guibg=NONE", tostring(count), color)
-	count = count + 1
-	vim.cmd(vim_command)
-	return "%#" .. tostring(count - 1)
-end
-function convert_ansi_tree_to_vim_hl_tree(file_path)
-	-- converts a textfile into a alphanvim dashboard
-	local file = io.open(file_path, "w+")
-	os.execute("pybonsai > " .. file_path)
-	io.close(file)
-	file = io.open(file_path, "r")
-	local dashboard = {}
-	local onfirstline = true
-	for line in file:lines() do
-		if onfirstline then
-			onfirstline = false
-			goto continue
-		end
-		table.insert(dashboard, "")
-		local i = 0
-		while i <= #line do
-			if line[i] == " " then
-				dashboard[#dashboard] = dashboard[#dashboard] .. line[i]
-				i = i + 1
-			elseif line[i] == "" then
-				i = i + 2
-				local asni_code = ""
-				while line[i] ~= "m" do
-					asni_code = asni_code .. line[i]
-					i = i + 1
-				end
-				i = i + 1
-				local hex_code = ansi_truecolor_to_hex(asni_code)
-				local hl_name = hex_to_vim_highlight(hex_code)
-				dashboard[#dashboard] = dashboard[#dashboard] .. hl_name
+	local leftmost_col, rightmost_col = math.huge, 0
+	-- Find the first and second-last lines containing the escape character
+	for i = 2, #input_lines - 1 do
+		if input_lines[i]:find(escape_char) then
+			local first_non_whitespace = input_lines[i]:find(escape_char)
+			leftmost_col = math.min(leftmost_col, first_non_whitespace)
+			rightmost_col = math.max(rightmost_col, #input_lines[i])
+			if first_escape_line == -1 then
+				first_escape_line = i
 			end
+			last_escape_line = i
 		end
-		::continue::
 	end
+
+	-- Determine the leftmost and rightmost points of the tree
+	-- Trim the lines to the leftmost and rightmost boundaries
+	local trimmed_lines = {}
+	for i = first_escape_line, last_escape_line - 1 do
+		local trimmed_line = input_lines[i]:sub(leftmost_col, rightmost_col)
+		table.insert(trimmed_lines, trimmed_line)
+	end
+
+	-- Write the trimmed lines to the output file
+	for _, line in ipairs(trimmed_lines) do
+		output_file:write(line .. "\n")
+	end
+	return trimmed_lines
 end
 
-local buff_id = vim.api.nvim_get_current_buf()
-local function highlight_alpha_dashboard(buf_id)
-	-- Assuming you want to loop through all lines in the dashboard
-	local lines = vim.api.nvim_buf_get_lines(buf_id, 0, -1, false)
+-- Open the output file for writing
+local file2 = io.open("tree2.txt", "w+")
+local function get_file_dimensions(file_path)
+	local file = io.open(file_path, "r")
+	local width = 0
+	local height = 0
 
-	for line_num, line in ipairs(lines) do
-		-- Loop through each character in the line
-		for col_num = 0, #line do
-			-- Example: Highlight each character in a different color
-			vim.api.nvim_buf_add_highlight(buf_id, -1, "Function", line_num - 1, col_num, col_num + 1)
+	for line in file:lines() do
+		height = height + 1
+		local line_width = #line
+		if line_width > width then
+			width = line_width
 		end
 	end
+
+	file:close()
+	return width
+end
+
+local function get_file_dimensions2(file_path)
+	local file = io.open(file_path, "r")
+	local width = 0
+	local height = 0
+
+	for line in file:lines() do
+		height = height + 1
+	end
+
+	file:close()
+	return height
 end
 
 return {
-	"goolord/alpha-nvim",
+
+	"nvimdev/dashboard-nvim",
 	event = "VimEnter",
 	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-
-		local file = io.open("tree.txt", "w+")
-		os.execute("pybonsai > tree.txt")
-		io.close(file)
-		-- Set header
-		dashboard.section.header.val = {
-			"123456789",
-		}
-
-		dashboard.opts = { { hl = "Function", 1, 9 } }
-
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("%", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC <C-r>", "󰁯  > Restore Session", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
-		}
-
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
-
-		highlight_alpha_dashboard(buff_id)
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+		require("dashboard").setup({
+			theme = "doom",
+			hide = {
+				statusline = true,
+				tabline = true,
+				winbar = true,
+			},
+			config = {
+				header = strip_tree_boundaries_to_file(lines, file2),
+				io.close(file2),
+				disable_move = true,
+				center = {
+					{
+						icon = " 󰈔 \t",
+						icon_hl = "DashboardIcon",
+						desc = "Open file",
+						desc_hl = "DashboardDesc",
+						key = "1",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Telescope find_files",
+					},
+					{
+						icon = " 󰈞 \t",
+						icon_hl = "Title",
+						desc = "Fuzzy Grep",
+						desc_hl = "DashboardDesc",
+						key = "2",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Telescope live_grep",
+					},
+					{
+						icon = " 󰂺 \t",
+						icon_hl = "Title",
+						desc = "Open Readme",
+						desc_hl = "DashboardDesc",
+						key = "3",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Glow README.md",
+					},
+					{
+						icon = "  \t",
+						icon_hl = "DashboardIcon",
+						desc = "Git History",
+						desc_hl = "DashboardDesc",
+						key = "4",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Telescope git_commits",
+					},
+					{
+						icon = "  \t",
+						icon_hl = "Title",
+						desc = "Mason Config",
+						desc_hl = "DashboardDesc",
+						key = "5",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Mason",
+					},
+					{
+						icon = "  \t",
+						icon_hl = "Title",
+						desc = "LazyVim Config",
+						desc_hl = "DashboardDesc",
+						key = "6",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Lazy",
+					},
+					{
+						icon = "  \t",
+						icon_hl = "Title",
+						desc = "Vim Options",
+						desc_hl = "DashboardDesc",
+						key = "7",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "Telescope vim_options",
+					},
+					{
+						icon = " 󰈆 \t",
+						icon_hl = "Title",
+						desc = "Quit NeoVim",
+						desc_hl = "DashboardDesc",
+						key = "0",
+						key_hl = "Number",
+						key_format = " %s.", -- `%s` will be substituted with value of `key`
+						action = "qa",
+					},
+				},
+			},
+		})
 	end,
+
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
 }
