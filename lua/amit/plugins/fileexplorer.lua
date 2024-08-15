b6fdd13d456e6a18c9894b18c490081241121b88
Amit-Weis <amitweisgor@gmail.com>
2024-08-14 23:16:53 -0400

    more config work

---
+++ lua/amit/plugins/fileexplorer.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	dependencies = { "nvim-telescope/telescope-file-browser.nvim" },
+
+	config = function()
+		local telescope = require("telescope")
+
+		-- Load the Telescope extension
+		telescope.setup({
+			extensions = {
+				file_browser = {
+					-- Configurations for file_browser extension
+					theme = "ivy",
+					hijack_netrw = true, -- Disable netrw in favor of file_browser
+				},
+			},
+		})
+
+		-- Load the file browser extension
+		telescope.load_extension("file_browser")
+
+		-- Keymap to open file browser
+	end,
+}
---
+++ lua/amit/plugins/undotree.lua
@@ -0,0 +1,24 @@
+return {
+	"mbbill/undotree",
+
+	config = function()
+		-- Set keymap for toggling Undotree
+		vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+
+		-- Configuration options for undotree
+		vim.g.undotree_WindowLayout = 2 -- Layout option, you can customize this
+		vim.g.undotree_SplitWidth = 30 -- Width of the undotree window
+		vim.g.undotree_SetFocusWhenToggle = 1 -- Focus on the undotree window when opened
+
+		-- Enable persistent undo in Neovim
+		vim.opt.undofile = true -- Maintain undo history between sessions
+		vim.opt.undodir = vim.fn.expand("~/.config/nvim/undo") -- Directory to store undo files
+
+		-- Ensure the undo directory exists
+		local undodir = vim.opt.undodir:get()[1]
+		if vim.fn.isdirectory(undodir) == 0 then
+			vim.fn.mkdir(undodir, "p")
+		end
+	end,
+}
+-- test
